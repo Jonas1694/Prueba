ba0826062f5e6294b903ce1b09c6185beab91cc0
Jonathan <Jonathan@DESKTOP-DFU7JJF>
2019-08-13 14:40:20 -0400

    sincronization con el repository

---
+++ EdwinP/ArquitecturaModel/Model/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArquitecturaModel.Model
+{
+    public class Productos
+    {
+        public string ProductosId { get; set; }
+        public string ProveedoresId { get; set; }
+        public Proveedores Proveedores { get; set; }
+        public string Codigo { get; set; }
+        public string NombreDelProducto { get; set; }
+        public string CantidadDelProducto { get; set; }
+         
+
+    }
+}
---
+++ EdwinP/ArquitecturaModel/Model/Proveedores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArquitecturaModel.Model
+{
+    public class Proveedores
+    {
+        public string ProveedoresId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string  Email { get; set; }
+        public string NombreDelaEmpresa { get; set; }
+
+    }
+}
